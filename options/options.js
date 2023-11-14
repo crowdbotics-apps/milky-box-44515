05f5dfa4772a21a9b0c4407410e7c24ddefd070d
Crowdbotics <team@crowdbotics.com>
2023-11-14 06:03:51 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "milky-box-44515",
+  url: "https://milky-box-44515.botics.co",
+  api: "https://milky-box-44515.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
